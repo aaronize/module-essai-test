c3d3f15b11aa51c21accaf18d77b755317decab4
aaron chen <aaron.chen@ucloud.cn>
2020-01-19 11:48:51 +0800

    add

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aaronize/module-essai-test
+
+go 1.12
+
+require github.com/aaronize/module-essai v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/aaronize/module-essai/base"
+)
+
+func main() {
+
+	base.Func2()
+
+	return
+}
